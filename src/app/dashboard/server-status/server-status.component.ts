c9544730efd4e54f997be5172b205c2c8e6fa89c
amar.somani <amar.somani@cohesity.com>
2024-12-25 14:50:48 +0530

    feat: how to use destroyRef instead of ngOnDestroy lifecycle hook

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
+
+  // If you don't want to use the ngOnDestroy lifecycle hooks, you can use the destryRef instead:
+  private destroyRef = inject(DestroyRef);
+
+  // constructor() {
+  //   setInterval(() => {
+  //     const rnd = Math.random();
+  //     this.currentStatus =
+  //       rnd < 0.5 ? 'online' : rnd < 0.9 ? 'offline' : 'unknown';
+  //   }, 5000);
+  // }
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const rnd = Math.random();
+      this.currentStatus =
+        rnd < 0.5 ? 'online' : rnd < 0.9 ? 'offline' : 'unknown';
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(this.interval);
+    });
+  }
+
+  // ngOnDestroy() {
+  //   if (this.interval) {
+  //     clearInterval(this.interval);
+  //   }
+  // }
 }
